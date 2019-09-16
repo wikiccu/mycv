f80323eaf3cb027e64186126985b45733d3a522d
Saeed <privatemx.2010@gmail.com>
2019-09-16 22:44:01 +0430

    link bootstrap and change UI

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,10 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
+$(function(){
+    $('.selectpicker').selectpicker();
+});
+
 (function($) {
 
 	var	$window = $(window),
